496efc4b7ababd636da86f19195e06b87bb308c1
AngeloDamante <angelo.damante16@gmail.com>
2021-07-14 03:15:44 +0200

    update GPIOLib.c

--- workspace-VxWorks/GPIOLib.c
+++ workspace-VxWorks/GPIOLib.c
@@ -6,16 +6,6 @@
 #include <subsys/gpio/vxbGpioLib.h>
 #include "GPIOLib.h"
 
-#ifdef GPIO_SYSCALL
-#include <private/syscallLibP.h>
-#include <syscall.h>
-#include <syscallLib.h>
-#include <private/syscallLibP.h>
-
-#define GPIO_GROUP            7
-#define GPIO_GROUP_NAME       "gpioGroup"
-#endif /* GPIO_SYSCALL */
-
 /*******************************************************************************
  * 
  * gpioSetVal - Set the value of the specified pin
@@ -74,46 +64,3 @@ int gpioGetDir(gpioSetType * gSet)
     return OK;
 }
 
-#ifdef GPIO_SYSCALL
-_WRS_DATA_ALIGN_BYTES(16) SYSCALL_RTN_TBL_ENTRY tempTbl[] =
-    {
-    SYSCALL_DESC_ENTRY (gpioSetVal, "GpioSet", 2),
-    SYSCALL_DESC_ENTRY (gpioGetVal, "GpioGet", 2),
-    SYSCALL_DESC_ENTRY (gpioSetDir, "GpioSetDir", 2),
-    SYSCALL_DESC_ENTRY (gpioGetDir, "GpioGetDir", 2),
-    };
-
-STATUS gpioGroup(void)
-    {
-    if (syscallGroupRegister (GPIO_GROUP, GPIO_GROUP_NAME,
-                              4, tempTbl, TRUE) == ERROR)
-        {
-         printf( "GPIO syscall registration FAILED");
-         return ERROR;
-    }
-    
-    printf( "GPIO syscall registration SUCCEEDED");
-    return OK;
-    }
-
-void registerGpioSyscalls(void) 
-    {
-    gpioGroup();
-    }
-
-
-STATUS syscallRegistrationHook
-    (
-    int                      groupNum,     /* group number */
-    char *                   groupName,    /* group name */
-    int                      numRoutines,  /* num routines in group */
-    SYSCALL_RTN_TBL_ENTRY ** ppRoutineTbl  /* addr of routine table */
-    );
-
-STATUS syscallRegisterHookAdd
-    (
-    SYSCALL_REGISTER_HOOK startMyDkm, /* routine to call upon group registration */
-    BOOL                  addToHead   /* add routine to head of list */
-    );
-
-#endif /* GPIO_SYSCALL */
